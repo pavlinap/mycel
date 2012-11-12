2704f7cdbe3a2d10b32f09cba0ac6321d6e3fcf4
Stefan Negele <mail@stefannegele.de>
2012-11-12 22:31:49 +0100

    reduced code

--- web/js/autoStorage.js
+++ web/js/autoStorage.js
@@ -4,10 +4,6 @@
  * 
  */
 
-(function() {
-    // pre-load functions here
-    })();
-
 window.onload = function() {
     localStorage["forumXML"] = document.getElementById("forumXML").innerHTML;
 //console.log(localStorage["forumXML"]);
