423706e6edfc844b80e59cd4cd9979407e626634
Martin <Martin@Martin-PC>
2012-11-27 23:32:27 +0100

    Added Link Detection

---
+++ src/java/org/shadownet/data/StringChange.java
@@ -0,0 +1,29 @@
+package org.shadownet.data;
+
+import java.lang.Object;
+import java.lang.String;
+import java.lang.StringBuilder;
+
+//import java.util.String;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Martin
+ */
+public class StringChange{
+    
+    public StringChange(){
+    }
+    
+    public String DetectLink(String s){
+        s = s.replaceAll(".\\swww|^www", " http://www");
+        s = s.replaceAll("(?<http>(http:[/][/]|www.)([a-z]|[A-Z]|[0-9]|[/.]|[~])*)", "<a target=\"_blank\" href=\"$0\">$0</a>");
+        return s;
+    }
+}
